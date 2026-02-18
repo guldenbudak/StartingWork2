b051790e41bb7ecebdb933646d21dd42fcaef8b9
Gülden BUDAK <guldenbudak0@gmail.com>
2026-02-18 14:38:35 +0300

    Update h2s22.py

--- h2s22.py
+++ h2s22.py
@@ -1,8 +1,9 @@
-toplam = 0
 sayi1 =int(input("İlk sayıyı giriniz :"))
 sayi2 =int(input("İkinci sayıyı giriniz :"))
-for sayi in range(sayi1,sayi2):
-    if x % sayi1 == 0 and x % sayi2 == 0:
-        toplam += x
-if toplam == x:
-    print(x)
+for a in range(sayi1,sayi2+1):
+    toplam = 0
+    for b in range(1,a):
+        if a %  b == 0:
+            toplam = toplam + b
+    if toplam == a:
+        print(a)
